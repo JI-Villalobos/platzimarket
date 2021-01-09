22844d65ee2e8bf3fb0c7e03e8e1a6f03878fc7b
Chuy Villalobos <chuy12villalobos@gmail.com>
2021-01-08 19:50:32 -0600

    start repo

---
+++ src/main/java/com/platzimarket/PlatzimarketApplication.java
@@ -0,0 +1,13 @@
+package com.platzimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatzimarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatzimarketApplication.class, args);
+	}
+
+}
