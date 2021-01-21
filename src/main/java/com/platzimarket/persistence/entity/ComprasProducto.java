a78ec83e88800065f483891d13592262b2b5452b
Chuy Villalobos <chuy12villalobos@gmail.com>
2021-01-20 19:53:46 -0600

    Purchase repository

--- src/main/java/com/platzimarket/persistence/entity/ComprasProducto.java
+++ src/main/java/com/platzimarket/persistence/entity/ComprasProducto.java
@@ -51,6 +51,7 @@ public class ComprasProducto {
     }
 
     @ManyToOne
+    @MapsId("idCompra")
     @JoinColumn(name = "id_compra", insertable = false, updatable = false)
     private Compra compra;
 
