8f7412644a13b67b44df25df9926eaa699e7461f
Chuy Villalobos <chuy12villalobos@gmail.com>
2021-01-21 22:11:38 -0600

    Swagger documentation

--- src/main/java/com/platzimarket/web/controller/PurchaseController.java
+++ src/main/java/com/platzimarket/web/controller/PurchaseController.java
@@ -2,6 +2,10 @@ package com.platzimarket.web.controller;
 
 import com.platzimarket.domain.Purchase;
 import com.platzimarket.domain.service.PurchaseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +21,27 @@ public class PurchaseController {
     PurchaseService purchaseService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket purchases")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Purchase>> getAll(){
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/client/{clientId}")
-    public ResponseEntity<List<Purchase>> getByCategory(@PathVariable("clientId") String clientId){
+    @ApiOperation("Search purchases by id")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "Purchase not found"),
+    })
+    public ResponseEntity<List<Purchase>> getByCategory(@ApiParam(value = "Purchase id", required = true, example = "1") @PathVariable("clientId") String clientId){
         return purchaseService.getByClient(clientId)
                 .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping ("/save")
+    @ApiOperation("Post a new purchase")
+    @ApiResponse(code = 201, message = "Purchse created succesfully")
     public ResponseEntity save(@RequestBody Purchase purchase){
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
     }
